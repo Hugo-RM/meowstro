0a2cdf4c2b2c726c551b1fca89d84168a29f7e2b
jai-serr-ace <xilverscott@gmail.com>
2025-04-26 09:51:18 -0700

    Update meowstro.cpp

--- src/meowstro.cpp
+++ src/meowstro.cpp
@@ -4,7 +4,8 @@
 #include <SDL_ttf.h>
 #include "RenderWindow.hpp"
 #include "Entity.hpp"
-#include "Audio.hpp
+#include "Audio.hpp"
+
 
 int main(int argc, char *args[])
 {
@@ -25,39 +26,40 @@ int main(int argc, char *args[])
 	RenderWindow window("Meowstro v1.0", windowWidth, windowHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	
 #ifdef CI_BUILD
-	SDL_Delay(5000);
+	SDL_Delay(3000);
 	SDL_Quit();
 	return 0;
 #endif
 	
 	const int NUM_FISH_TEXTURES = 9;
-	SDL_Texture* fishTextures[NUM_FISH_TEXTURES];
+	//SDL_Texture* fishTextures[NUM_FISH_TEXTURES];
 
-	fishTextures[0] = window.loadTexture("../assets/images/blue-fish/fish-1.png");
-	fishTextures[1] = window.loadTexture("../assets/images/blue-fish/fish-2.png");
-	fishTextures[2] = window.loadTexture("../assets/images/blue-fish/fish-3.png");
-	fishTextures[3] = window.loadTexture("../assets/images/green-fish/fish-1.png");
-	fishTextures[4] = window.loadTexture("../assets/images/green-fish/fish-2.png");
-	fishTextures[5] = window.loadTexture("../assets/images/green-fish/fish-3.png");
-	fishTextures[6] = window.loadTexture("../assets/images/gold-fish/fish-1.png");
-	fishTextures[7] = window.loadTexture("../assets/images/gold-fish/fish-2.png");
-	fishTextures[8] = window.loadTexture("../assets/images/gold-fish/fish-3.png");
+	//fishTextures[0] = window.loadTexture("../assets/images/blue-fish/fish-1.png");
+	//fishTextures[1] = window.loadTexture("../assets/images/blue-fish/fish-2.png");
+	//fishTextures[2] = window.loadTexture("../assets/images/blue-fish/fish-3.png");
+	//fishTextures[3] = window.loadTexture("../assets/images/green-fish/fish-1.png");
+	//fishTextures[4] = window.loadTexture("../assets/images/green-fish/fish-2.png");
+	//fishTextures[5] = window.loadTexture("../assets/images/green-fish/fish-3.png");
+	//fishTextures[6] = window.loadTexture("../assets/images/gold-fish/fish-1.png");
+	//fishTextures[7] = window.loadTexture("../assets/images/gold-fish/fish-2.png");
+	//fishTextures[8] = window.loadTexture("../assets/images/gold-fish/fish-3.png");
 
-	Entity fishEntities[8] = { Entity(180, 570, fishTextures[0]),
-							   Entity(408, 570, fishTextures[1]),
-							   Entity(636, 570, fishTextures[2]),
-							   Entity(864, 570, fishTextures[3]),
-							   Entity(1092, 570, fishTextures[4]),
-							   Entity(1092, 410, fishTextures[5]),
-							   Entity(1092, 250, fishTextures[6]),
-							   Entity(1092, 90, fishTextures[7]) };
+	//Entity fishEntities[8] = { Entity(180, 570, fishTextures[0]),
+	//						   Entity(408, 570, fishTextures[1]),
+	//						   Entity(636, 570, fishTextures[2]),
+	//						   Entity(864, 570, fishTextures[3]),
+	//						   Entity(1092, 570, fishTextures[4]),
+	//						   Entity(1092, 410, fishTextures[5]),
+	//						   Entity(1092, 250, fishTextures[6]),
+	//						   Entity(1092, 90, fishTextures[7]) };
 
 	bool gameRunning = true;
-
 	SDL_Event event;
-
+	Audio player;
+	player.playBackgroundMusic("C:/Users/xxsha/source/repos/Hugo-RM/meowstro/assets/audio/mymarch.mp3");
 	while (gameRunning)
 	{
+		
 		while (SDL_PollEvent(&event))
 		{
 			switch(event.type)
@@ -78,13 +80,15 @@ int main(int argc, char *args[])
 		// window.render(background);
 		for (int i = 0; i < 8; i++)
 		{
-			window.render(fishEntities[i]);
+	/*		window.render(fishEntities[i]);*/
 		}
-
+		
 		window.display();
+		
 	}
-
+	player.stopBackgroundMusic();
 	window.~RenderWindow();
+	player.~Audio();
 	SDL_Quit();
 
 
